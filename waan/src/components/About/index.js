77c2b03e04cc99e731adf750eee09e83e1eabd02
PIYACHAT MONGKOLPATTHANAKUL <taeaew.w@outlook.com>
2017-10-24 17:11:38 +0700

    create project

---
+++ waan/src/components/About/index.js
@@ -0,0 +1,16 @@
+
+// src/components/App/index.js
+import React, { Component } from 'react';
+import './style.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Page</h1>
+      </div>
+    )
+  }
+}
+
+export default About;
